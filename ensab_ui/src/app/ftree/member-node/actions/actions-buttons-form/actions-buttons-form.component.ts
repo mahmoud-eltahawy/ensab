20daac84b73b298bc1d23ade2c5ae14a29ac1c59
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-04-08 17:36:08 +0200

    extract button from actions

--- ensab_ui/src/app/ftree/member-node/actions/actions-buttons-form/actions-buttons-form.component.ts
+++ ensab_ui/src/app/ftree/member-node/actions/actions-buttons-form/actions-buttons-form.component.ts
@@ -1,10 +1,12 @@
 import { Component, input } from "@angular/core";
 import Member from "../../member";
+import { ActionButtonComponent } from "./action-button-component";
+
 
 @Component({
   selector: "actions-buttons",
   standalone: true,
-  imports: [],
+  imports: [ActionButtonComponent],
   templateUrl: "./actions-buttons-form.component.html",
 })
 export class ActionsButtonsFormComponent {
